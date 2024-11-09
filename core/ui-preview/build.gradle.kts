3aeb1bdf142a867ccccbe9a8332c29c238bc73d6
DEV2-032 <yournewdev@bnpparibasfortis.com>
2024-11-09 20:00:56 +0100

    feat(ui components): setup ui module & ui-preview

--- core/ui-preview/build.gradle.kts
+++ core/ui-preview/build.gradle.kts
@@ -2,25 +2,24 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.screenshot)
     id("be.bnp.katas.tictactoe.androidlibrary")
 }
 
 android {
     namespace = "be.bnp.katas.tictactoe.ui"
 
-    buildFeatures {
-        compose = true
-    }
+    experimentalProperties["android.experimental.enableScreenshotTest"] = true
 }
 
 dependencies {
+    implementation(project(":core:ui"))
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
-    implementation(libs.compose.ui.graphics)
     implementation(libs.compose.ui.tooling)
-    implementation(libs.compose.material3)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    screenshotTestImplementation(libs.compose.ui.tooling)
 }
