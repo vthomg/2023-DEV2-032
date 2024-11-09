95064b712d702d4cebba3188529056f743790eaf
DEV2-032 <yournewdev@bnpparibasfortis.com>
2024-11-09 14:30:43 +0100

    feat(victory cases): failed test - row victory is detected

---
+++ core/domain/src/main/java/be/bnp/katas/tictactoe/domain/usecase/IsRowVictoryUseCase.kt
@@ -0,0 +1,27 @@
+package be.bnp.katas.tictactoe.domain.usecase
+
+import be.bnp.katas.tictactoe.data.model.BoardPoint
+import be.bnp.katas.tictactoe.domain.repository.BoardRepository
+
+/**
+ * Check if the user won for the row
+ * If there are 3 in a row, then the user wins
+ */
+class IsRowVictoryUseCase(
+    boardRepository: BoardRepository,
+) {
+    private val boardPoints = boardRepository.boardPoints
+
+    operator fun invoke(point: BoardPoint): Boolean {
+        val (_, column) = point
+
+        for (rowIndex in boardPoints.indices) {
+            val pointOnBoard = boardPoints[rowIndex][column]
+            // Quit the loop, user did not won already since there is already different point
+            if (pointOnBoard.state != BoardPoint.State.EMPTY && pointOnBoard.state != point.state) break
+            // If iteration went till the last element with no breaks, user won
+            else if (rowIndex == boardPoints.size - 1) return true
+        }
+        return false
+    }
+}
---
+++ core/domain/src/test/java/be/bnp/katas/tictactoe/domain/usecase/IsRowVictoryUseCaseTest.kt
@@ -0,0 +1,34 @@
+package be.bnp.katas.tictactoe.domain.usecase
+
+import be.bnp.katas.tictactoe.data.BoardPoints
+import be.bnp.katas.tictactoe.data.model.BoardPoint
+import be.bnp.katas.tictactoe.domain.repository.BoardRepositoryImpl
+import be.bnp.katas.tictactoe.domain.utils.asBoardPoints
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class IsRowVictoryUseCaseTest {
+    companion object {
+        fun useCaseForBoardPoints(boardPoints: BoardPoints): IsRowVictoryUseCase {
+            val repositoryMocked = mockk<BoardRepositoryImpl>()
+            every { repositoryMocked.boardPoints } returns boardPoints
+            return IsRowVictoryUseCase(repositoryMocked)
+        }
+    }
+
+    @Test
+    fun `Row Victory is detected`() {
+        val givenBoard = """
+            x,_,x
+            o,x,x
+            _,o,o
+        """.trimIndent()
+        val givenPoint = BoardPoint(row = 2, column = 0, BoardPoint.State.NOUGHT)
+
+        val useCase = useCaseForBoardPoints(givenBoard.asBoardPoints)
+
+        assertTrue(useCase(givenPoint))
+    }
+}
