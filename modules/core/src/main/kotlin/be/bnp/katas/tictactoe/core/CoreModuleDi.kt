46bae3bab8d5d16734b6eade62ba1d173ada8df8
DEV2-032 <yournewdev@bnpparibasfortis.com>
2024-11-10 11:05:01 +0100

    feat(koin): passed test - coroutine dispatcher is declared

--- modules/core/src/main/kotlin/be/bnp/katas/tictactoe/core/CoreModuleDi.kt
+++ modules/core/src/main/kotlin/be/bnp/katas/tictactoe/core/CoreModuleDi.kt
@@ -1,6 +1,10 @@
 package be.bnp.katas.tictactoe.core
 
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 internal val coreModuleDi = module {
+    single {
+        Dispatchers.IO
+    }
 }
