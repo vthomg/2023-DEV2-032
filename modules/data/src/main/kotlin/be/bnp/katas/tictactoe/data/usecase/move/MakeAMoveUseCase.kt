ddfe7547eee5194fa37b238ddfe8fc5ff5357006
DEV2-032 <yournewdev@bnpparibasfortis.com>
2024-11-11 10:47:16 +0100

    feat(use case): passed test - make a move use case does not updates the board for invalid point

--- modules/data/src/main/kotlin/be/bnp/katas/tictactoe/data/usecase/move/MakeAMoveUseCase.kt
+++ modules/data/src/main/kotlin/be/bnp/katas/tictactoe/data/usecase/move/MakeAMoveUseCase.kt
@@ -6,7 +6,10 @@ import be.bnp.katas.tictactoe.data.repository.BoardRepository
 class MakeAMoveUseCase(
     private val boardRepository: BoardRepository,
 ) {
-    operator fun invoke(point: BoardPoint) {
+    operator fun invoke(point: BoardPoint): Boolean {
+        if (!boardRepository.isPointValidForBoard(point)) return false
+
         boardRepository.updatePoint(point)
+        return true
     }
 }
--- modules/data/src/test/kotlin/be/bnp/katas/tictactoe/data/usecase/move/MakeAMoveUseCaseTest.kt
+++ modules/data/src/test/kotlin/be/bnp/katas/tictactoe/data/usecase/move/MakeAMoveUseCaseTest.kt
@@ -4,6 +4,8 @@ import be.bnp.katas.tictactoe.data.model.BoardPoint
 import be.bnp.katas.tictactoe.data.repository.BoardRepository
 import be.bnp.katas.tictactoe.data.repository.BoardRepositoryImpl
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
@@ -30,7 +32,7 @@ class MakeAMoveUseCaseTest {
     fun `Make a move updates the point on the board`() {
         val givenPoint = BoardPoint(row = 1, column = 2, BoardPoint.State.CROSS)
 
-        makeAMoveUseCase(givenPoint)
+        assertTrue(makeAMoveUseCase(givenPoint))
 
         val (row, column) = givenPoint
         assertEquals(givenPoint, boardRepository.boardPoints[row][column])
@@ -40,9 +42,6 @@ class MakeAMoveUseCaseTest {
     fun `Make a move does not updates the invalid point on the board`() {
         val givenPoint = BoardPoint(row = 1, column = 10, BoardPoint.State.CROSS)
 
-        makeAMoveUseCase(givenPoint)
-
-        val (row, column) = givenPoint
-        assertEquals(givenPoint, boardRepository.boardPoints[row][column])
+        assertFalse(makeAMoveUseCase(givenPoint))
     }
 }
